2c2b683cc04986a4809650e2d5a72f664023c116
Ivan Filipović <ivan.filipovic.fila@gmail.com>
2018-09-20 21:09:38 +0200

    add scraper script

---
+++ scraper.py
@@ -0,0 +1,33 @@
+import urllib.request
+import urllib.parse
+import re
+import os
+
+# extract all sources from given tag and save to file
+def get_src(url, tag):
+	data = _get_data(url)
+	images = re.findall(r'<'+tag+' src="(.*?)"', str(data))
+	_save_to_file(images)
+	return images
+
+# get contents of given tag
+def get_tag_content(url, tag):
+	data = _get_data(url)
+	titles = re.findall(r'<'+tag+'>(.*?)</'+tag+'>', str(data))
+	_save_to_file(titles)
+	return titles
+
+# get basic request and fetch data
+def _get_data(url):
+	req = urllib.request.Request(url)
+	rsp = urllib.request.urlopen(req)
+	rspData = rsp.read()
+	return rspData
+
+# save results to file, every entry on new line
+def _save_to_file(res):
+	os.remove('results.txt')
+	with open('results.txt', 'a') as file:
+		for line in res:
+			file.write(line + '\n')
+		file.close()
